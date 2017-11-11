5e571d8029e2c533989967c1d08fb4589cdc29ca
aabedi <aabedi7@gmail.com>
2017-11-10 20:51:18 -0500

    Node server working, need to fix html rendering issue - stuck on defined index. Also need to fix backend facing js code.

--- app.js
+++ app.js
@@ -1,7 +1,30 @@
-var port = process.env.PORT || 3000,
+var express = require("express");
+var login = require('./routes/login_routes');
+var bodyParser = require('body-parser');
+
+var app = express();
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+
+
+var router = express.Router();
+// test route
+router.get('/', function(req, res) {
+    res.json({ message: 'welcome to our upload module apis' });
+});
+
+
+
+var port = process.env.PORT || 8081,
     http = require('http'),
     fs = require('fs'),
-    html = fs.readFileSync('index.html');
+    html = fs.readFileSync('./views/login.html');
 
 var log = function(entry) {
     fs.appendFileSync('/tmp/sample-app.log', new Date().toISOString() + ' - ' + entry + '\n');
---
+++ knexfile.js
@@ -0,0 +1,11 @@
+/**
+ * Created by Albert on 11/10/17.
+ */
+module.exports = {
+    client: 'mysql',
+    connection: {
+        user: 'dbuser',
+        password: 'alphabetagamma',
+        database: 'awsEcrop'
+    }
+}
