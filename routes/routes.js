45549a3491f12f82768aca29ae43a6878f110dfb
Albert Abedi <aabedi3@gatech.edu>
2017-11-13 00:59:42 -0500

    housekeeping, still working on typeError bade gateway error

--- routes/routes.js
+++ routes/routes.js
@@ -57,7 +57,7 @@ module.exports = function(app, passport) {
     // we will want this protected so you have to be logged in to visit
     // we will use route middleware to verify this (the isLoggedIn function)
     app.get('/index', isLoggedIn, function(req, res) {
-        res.render('index.html', {
+        res.render('/public/views/index.html', {
             user : req.user // get the user out of session and pass to template
         });
     });
